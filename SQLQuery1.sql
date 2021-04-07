ce3d32c74ae0104e6b89e5db08e7ee1075f3291c
Sang-2000 <57800428+Sang-2000@users.noreply.github.com>
2021-04-07 23:35:48 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+Use Nawab
+
+create table Student (
+	Id int identity primary key,
+	f_name nvarchar(100),
+	m_name nvarchar(100),
+	l_name nvarchar(100),
+	address nvarchar(100),
+	birthDate nvarchar(100),
+	score nvarchar(100),
+	dep_id nvarchar(100),
+)
+
+select *
+from dbo.Student
+
+insert into Student (f_name, m_name, l_name, address, birthDate, score) values (@f_name, @m_name, @l_name, @address, @birthDate, @score)
